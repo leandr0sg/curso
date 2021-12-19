847cb2c9425f0f74b9e7cdaa42474fc3e86f9126
Leandro <gomesleandrodossantos@gmail.com>
2021-12-19 19:49:30 -0300

    Projeto criado

---
+++ src/test/java/com/leandro/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
